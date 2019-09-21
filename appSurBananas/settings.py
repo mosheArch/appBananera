38f7b5f92059bb8fcb0e030af5faf9739a0f1c8d
Mictlantecuhtli <vali@localhost.localdomain>
2019-09-20 21:00:28 -0500

    Sqlite config

--- appSurBananas/settings.py
+++ appSurBananas/settings.py
@@ -77,14 +77,20 @@ WSGI_APPLICATION = 'appSurBananas.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#             'NAME': 'surBananera',
+#             'USER': 'vali',
+#             'PASSWORD': 'moses666',
+#             'HOST': 'localhost',
+#             'PORT': '3306',
+#     }
+# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-            'NAME': 'surBananera',
-            'USER': 'vali',
-            'PASSWORD': 'moses666',
-            'HOST': 'localhost',
-            'PORT': '3306',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
